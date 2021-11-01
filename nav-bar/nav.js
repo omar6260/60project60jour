d1ab2900ee4101e7e709f01a90e45ba9fd825e7c
oumar <foumar404@gmail.com>
2021-11-01 14:35:38 +0100

    fin project

---
+++ nav-bar/nav.js
@@ -0,0 +1,240 @@
+/*------------------------------------*\
+   HERO CSS
+\*------------------------------------*/
+
+
+.main [data-module-template="heroes"] [data-unit-id] .unit-copy-wrapper { padding-left: 15px; padding-right: 15px; }
+
+
+/* HERO 1 IPHONE-13-PRO */
+
+/* SCREENS */
+
+
+
+/* LOCAL FIXES */
+
+
+
+/* END HERO 1 IPHONE-13-PRO */
+
+
+
+
+/* HERO 2 IPHONE-13 */
+
+/* SCREENS */
+
+/* LOCAL FIXES */
+
+
+
+/* END HERO 2 IPHONE-13 */
+
+
+
+
+
+/* HERO 3 IPAD-PRO */
+
+/* SCREENS */
+
+/* LOCAL FIXES */
+
+/* END HERO 3 IPAD-PRO */
+
+
+/*------------------------------------*\
+  END HERO CSS
+\*------------------------------------*/
+
+
+
+
+/*------------------------------------*\
+   PROMO CSS
+\*------------------------------------*/
+
+
+/* PROMO 1 IMAC */
+
+/* SCREENS */
+
+/* LOCAL FIXES */
+
+/* END PROMO 1 IMAC */
+
+
+
+
+/* PROMO 2 AIRTAG */
+
+/* SCREENS */
+
+/* LOCAL FIXES */
+
+@media only screen and (max-width: 1068px) {
+	.main [data-module-template="promos"] [data-unit-id="airtag"] .unit-copy-wrapper .headline {
+		margin-top: 47px;
+	}
+}
+
+@media only screen and (max-width: 734px){
+	.main [data-module-template="promos"] [data-unit-id="airtag"] .unit-copy-wrapper .headline {
+		margin-top: 41px;
+	}
+}
+
+/* END PROMO 2 AIRTAG */
+
+
+
+
+/* PROMO 3 AIRPODS-MAX */
+
+/* SCREENS */
+
+/* LOCAL FIXES */
+
+/* END PROMO 3 AIRPODS-MAX */
+
+
+
+
+/* PROMO 4 IPAD */
+
+/* SCREENS */
+
+.logo-image-ipad-logo-tile {
+    width: 119px;
+    height: 98px;
+    background-size: 119px 98px;
+    background-repeat: no-repeat;
+    background-image: url("/euro/home/u/generic/images/logos/ipad/logo-tile__ec23lf7d072a_large.png");
+    background-position:center
+}
+
+@media only screen and (-webkit-min-device-pixel-ratio: 1.5), only screen and (min-resolution: 1.5dppx),(-webkit-min-device-pixel-ratio: 1.5),(min-resolution: 144dpi) {
+    .logo-image-ipad-logo-tile {
+        background-image: url("/euro/home/u/generic/images/logos/ipad/logo-tile__ec23lf7d072a_large_2x.png")
+    }
+}
+
+@media only screen and (max-width: 1068px) {
+    .logo-image-ipad-logo-tile {
+        width: 120px;
+        height: 98px;
+        background-size: 120px 98px;
+        background-repeat: no-repeat;
+        background-image: url("/euro/home/u/generic/images/logos/ipad/logo-tile__ec23lf7d072a_medium.png")
+    }
+}
+
+@media only screen and (max-width: 1068px) and (-webkit-min-device-pixel-ratio: 1.5), only screen and (max-width: 1068px) and (min-resolution: 1.5dppx), only screen and (max-width: 1068px) and (min-resolution: 144dpi) {
+    .logo-image-ipad-logo-tile {
+        background-image: url("/euro/home/u/generic/images/logos/ipad/logo-tile__ec23lf7d072a_medium_2x.png")
+    }
+}
+
+@media only screen and (max-width: 734px) {
+    .logo-image-ipad-logo-tile {
+        width: 119px;
+        height: 98px;
+        background-size: 119px 98px;
+        background-repeat: no-repeat;
+        background-image: url("/euro/home/u/generic/images/logos/ipad/logo-tile__ec23lf7d072a_small.png")
+    }
+}
+
+@media only screen and (max-width: 734px) and (-webkit-min-device-pixel-ratio: 1.5), only screen and (max-width: 734px) and (min-resolution: 1.5dppx), only screen and (max-width: 734px) and (min-resolution: 144dpi) {
+    .logo-image-ipad-logo-tile {
+        background-image: url("/euro/home/u/generic/images/logos/ipad/logo-tile__ec23lf7d072a_small_2x.png")
+    }
+}
+
+.main [data-module-template="promos"] .unit-image-wrapper .unit-image.unit-image-ipad-tile {
+    width: 388px;
+    height: 580px;
+    background-size: 388px 580px;
+    background-repeat: no-repeat;
+    background-image: url("/euro/home/u/screens_alt/images/promos/ipad/tile__cauwwcyyn9hy_large.jpg")
+}
+@media only screen and (-webkit-min-device-pixel-ratio: 1.5), only screen and (min-resolution: 1.5dppx),(-webkit-min-device-pixel-ratio: 1.5),(min-resolution: 144dpi) {
+    .main [data-module-template="promos"] .unit-image-wrapper .unit-image.unit-image-ipad-tile {
+        background-image: url("/euro/home/u/screens_alt/images/promos/ipad/tile__cauwwcyyn9hy_large_2x.jpg")
+    }
+}
+@media only screen and (max-width: 1068px) {
+    .main [data-module-template="promos"] .unit-image-wrapper .unit-image.unit-image-ipad-tile {
+        width: 292px;
+        height: 490px;
+        background-size: 292px 490px;
+        background-repeat: no-repeat;
+        background-image: url("/euro/home/u/screens_alt/images/promos/ipad/tile__cauwwcyyn9hy_medium.jpg")
+    }
+}
+@media only screen and (max-width: 1068px) and (-webkit-min-device-pixel-ratio: 1.5), only screen and (max-width: 1068px) and (min-resolution: 1.5dppx), only screen and (max-width: 1068px) and (min-resolution: 144dpi) {
+    .main [data-module-template="promos"] .unit-image-wrapper .unit-image.unit-image-ipad-tile {
+        background-image: url("/euro/home/u/screens_alt/images/promos/ipad/tile__cauwwcyyn9hy_medium_2x.jpg")
+    }
+}
+@media only screen and (max-width: 734px) {
+    .main [data-module-template="promos"] .unit-image-wrapper .unit-image.unit-image-ipad-tile {
+        width: 292px;
+        height: 548px;
+        background-size: 292px 548px;
+        background-repeat: no-repeat;
+        background-image: url("/euro/home/u/screens_alt/images/promos/ipad/tile__cauwwcyyn9hy_small.jpg")
+    }
+}
+@media only screen and (max-width: 734px) and (-webkit-min-device-pixel-ratio: 1.5), only screen and (max-width: 734px) and (min-resolution: 1.5dppx), only screen and (max-width: 734px) and (min-resolution: 144dpi) {
+    .main [data-module-template="promos"] .unit-image-wrapper .unit-image.unit-image-ipad-tile {
+        background-image: url("/euro/home/u/screens_alt/images/promos/ipad/tile__cauwwcyyn9hy_small_2x.jpg")
+    }
+}
+
+/* LOCAL FIXES */
+
+.main [data-module-template="promos"] [data-unit-id="ipad"] .unit-wrapper {
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: reverse;
+    -ms-flex-direction: column-reverse;
+    flex-direction: column-reverse
+}
+
+.main [data-module-template="promos"] [data-unit-id="ipad"] .unit-copy-wrapper .subhead {
+    max-width:290px
+}
+
+@media only screen and (max-width: 1068px) {
+    .main [data-module-template="promos"] [data-unit-id="ipad"] .unit-copy-wrapper .subhead {
+        max-width: 260px
+    }
+}
+
+.main [data-module-template="promos"] [data-unit-id="ipad"] .unit-copy-wrapper .cta-links {
+    margin-bottom:53px
+}
+
+@media only screen and (max-width: 1068px) {
+    .main [data-module-template="promos"] [data-unit-id="ipad"] .unit-copy-wrapper .cta-links {
+        margin-bottom:42px
+    }
+}
+
+@media only screen and (max-width: 734px) {
+    .main [data-module-template="promos"] [data-unit-id="ipad"] .unit-copy-wrapper .cta-links {
+        margin-bottom: 37px
+    }
+}
+
+html.text-zoom .main [data-module-template="promos"] [data-unit-id="ipad"] .unit-image-wrapper .unit-image-ipad-tile {
+    top: 0
+}
+
+/* END PROMO 4 IPAD */
+
+
+/*------------------------------------*\
+  END PROMO CSS
+\*------------------------------------*/
