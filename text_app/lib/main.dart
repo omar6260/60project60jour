fb9da94c1afd518fa1a90be301b0f0926ba4a7b7
Oumar <foumar404@gmail.com>
2021-12-07 13:43:55 +0100

    challeng 2 text

---
+++ text_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: TextExample()));
+}
+
+class TextExample extends StatelessWidget {
+  const TextExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Simple Text',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Colors.blue,
+        fontSize: 30.0,
+        fontStyle: FontStyle.italic,
+        decoration: TextDecoration.none,
+      ),
+    );
+  }
+}
