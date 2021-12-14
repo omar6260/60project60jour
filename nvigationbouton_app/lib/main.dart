46c596c5eb7a105bcda90fd14cdaa6646312ea8e
Oumar <foumar404@gmail.com>
2021-12-14 08:55:16 +0100

    bottomNvigation dy 5

---
+++ nvigationbouton_app/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int bottomSelectedIndex = 0;
+  String title = 'Home';
+  List<String> titles = ['Home', 'Product', 'Settings'];
+  PageController pageController = PageController(
+    initialPage: 0,
+    keepPage: true,
+  );
+  void pageChanged(int index) {
+    setState(() {
+      bottomSelectedIndex = index;
+    });
+  }
+
+  Widget buildPageView() {
+    return PageView(
+      controller: pageController,
+      onPageChanged: (index) {
+        pageChanged(index);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
